7a04ae2d3b774e951dbf7ec16e8284caa9a4fa03
protarie <protarie@gmail.com>
2023-02-21 12:16:05 +0900

    시간초과 수정

--- BOJ_5525/src/main.cpp
+++ BOJ_5525/src/main.cpp
@@ -4,57 +4,49 @@
 
 using namespace std;
 
+using vi = vector<int>;
+
 int main() {
-    ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-    int N;
-    cin >> N;
+	int n; cin >> n;
+	int m; cin >> m;
+	string s; cin >> s;
 
-    int M;
-    cin >> M;
+	auto pc = vi(m / 2, 0);
+	auto nl = int{ 0 };
+	for (auto i = 0; i < m - 2; ++i) {
+		if ('O' == s[i]) {
+			if (0 < nl) {
+				pc[nl - 1] += 1;
+			}
 
-    string S;
-    cin >> S;
+			nl = 0;
+			continue;
+		}
 
-    auto f = S.find("IOI");
-    if (S.npos != f) {
-        M -= f;
-        S = S.substr(S.find("IOI"));
+		if ("OI" == s.substr(i + 1, 2)) {
+			++nl;
+			i += 1;
+		}
+		else {
+			if (0 < nl) {
+				pc[nl - 1] += 1;
+			}
+			nl = 0;
+		}
+	}
 
-        vector<int> ps{};
-        f = S.find("I");
-        int p{ 0 };
-        int i{ 0 };
-        while (S.npos != f) {
-            if ("OI" == S.substr(f + i + 1, 2)) {
-                p += 1;
-                i += 2;
-            }
-            else {
-                ps.push_back(p);
-                p = 0;
+	if (0 < nl) {
+		pc[nl - 1] += 1;
+	}
 
-                S = S.substr(f + i + 1);
-                i = 0;
+	auto ans = int{ 0 };
+	for (auto i = n - 1; i < m / 2; ++i) {
+		ans += pc[i] * (i - (n - 1) + 1);
+	}
+	cout << ans;
 
-                f = S.find("I");
-            }
-        }
-
-        int result{ 0 };
-        for (const auto& p : ps)
-        {
-            if (N <= p) {
-                result += (p - N + 1);
-            }
-        }
-
-        cout << result;
-    }
-    else {
-        cout << 0;
-    }
-
-    return 0;
+	return 0;
 }
